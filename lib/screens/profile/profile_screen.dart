ca73bf85b97d6682f2184ee159dff7b7b9293f15
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2025-03-19 17:18:26 +0530

    Update profile_screen.dart

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../help_center/help_center_screen.dart';
 import '../settings/settings_screen.dart';
+import '../splash/splash_screen.dart'; // Added import for SplashScreen
 import 'components/profile_menu.dart';
 import 'components/profile_pic.dart';
 
@@ -9,6 +10,7 @@ class ProfileScreen extends StatelessWidget {
   static String routeName = "/profile";
 
   const ProfileScreen({super.key});
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,11 +50,47 @@ class ProfileScreen extends StatelessWidget {
             ProfileMenu(
               text: "Log Out",
               icon: "assets/icons/Log out.svg",
-              press: () {},
+              press: () {
+                // Show confirmation dialog
+                showLogoutConfirmationDialog(context);
+              },
             ),
           ],
         ),
       ),
+    );
+  }
+  
+  // Function to show a confirmation dialog before logging out
+  void showLogoutConfirmationDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Log Out"),
+          content: const Text("Are you sure you want to log out?"),
+          actions: <Widget>[
+            TextButton(
+              child: const Text("Cancel"),
+              onPressed: () {
+                Navigator.of(context).pop(); 
+              },
+            ),
+            TextButton(
+              child: const Text("Log Out"),
+              onPressed: () {
+                Navigator.of(context).pop(); 
+                
+                // Clear navigation stack and go to splash screen
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                  SplashScreen.routeName,
+                  (Route<dynamic> route) => false, 
+                );
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
