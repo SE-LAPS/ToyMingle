02219e19d27d06f97a984c69891ecebe2c30d292
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2025-03-20 04:33:34 +0530

    Update categories.dart

--- lib/screens/home/components/categories.dart
+++ lib/screens/home/components/categories.dart
@@ -3,6 +3,11 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shop_app/screens/flash_deal/flash_deal_screen.dart';
 import 'package:shop_app/screens/Game_deal/game_deal_screen.dart';
 import 'package:shop_app/screens/daily_gift_deal/daily_gift_screen.dart';
+// Use the same import strategy here as in routes.dart
+import 'package:shop_app/screens/swap/swap.dart' as swap_model;
+import 'package:shop_app/screens/swap/swap_screen.dart';
+import 'package:shop_app/screens/sell/sell_screen.dart';
+import 'package:shop_app/screens/child_lock/child_lock_screen.dart';
 
 class Categories extends StatelessWidget {
   const Categories({super.key});
@@ -13,8 +18,9 @@ class Categories extends StatelessWidget {
       {"icon": "assets/icons/Flash Icon.svg", "text": "Flash Deal"},     
       {"icon": "assets/icons/Game Icon.svg", "text": "Game"},
       {"icon": "assets/icons/Gift Icon.svg", "text": "Daily Gift"},
-      {"icon": "assets/icons/Bill Icon.svg", "text": "Bill"},
-      {"icon": "assets/icons/Discover.svg", "text": "More"},
+      {"icon": "assets/icons/Settings.svg", "text": "Swap"},
+      {"icon": "assets/icons/Cart Icon.svg", "text": "Sell"},
+      {"icon": "assets/icons/Lock.svg", "text": "Lock"},
     ];
     return Padding(
       padding: const EdgeInsets.all(20),
@@ -28,14 +34,17 @@ class Categories extends StatelessWidget {
             text: categories[index]["text"],
             press: () {
               if (categories[index]["text"] == "Flash Deal") {
-                Navigator.pushNamed(context,
-                    FlashDealScreen.routeName); // Use FlashDealScreen.routeName
-              }
-              if (categories[index]["text"] == "Game") {
+                Navigator.pushNamed(context, FlashDealScreen.routeName);
+              } else if (categories[index]["text"] == "Game") {
                 Navigator.pushNamed(context, GameDealScreen.routeName);
-              }
-               if (categories[index]["text"] == "Daily Gift") {
+              } else if (categories[index]["text"] == "Daily Gift") {
                 Navigator.pushNamed(context, DailyGiftScreen.routeName);
+              } else if (categories[index]["text"] == "Swap") {
+                Navigator.pushNamed(context, SwapScreen.routeName);
+              } else if (categories[index]["text"] == "Sell") {
+                Navigator.pushNamed(context, SellScreen.routeName);
+              } else if (categories[index]["text"] == "Lock") {
+                Navigator.pushNamed(context, ChildLockScreen.routeName);
               }
             },
           ),
