7c9180091b6d0a0a652e8152ea6c7c3cdb9ebf11
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2025-03-22 06:09:25 +0530

    Update color_dots.dart

--- lib/screens/details/components/color_dots.dart
+++ lib/screens/details/components/color_dots.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-
-import '../../../components/rounded_icon_btn.dart';
-import '../../../constants.dart';
 import '../../../models/Product.dart';
 
-class ColorDots extends StatelessWidget {
+class ColorDots extends StatefulWidget {
   const ColorDots({
     Key? key,
     required this.product,
@@ -13,65 +10,53 @@ class ColorDots extends StatelessWidget {
   final Product product;
 
   @override
+  State<ColorDots> createState() => _ColorDotsState();
+}
+
+class _ColorDotsState extends State<ColorDots> {
+  int selectedColor = 0;
+
+  @override
   Widget build(BuildContext context) {
-    // Now this is fixed and only for demo
-    int selectedColor = 3;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Row(
         children: [
           ...List.generate(
-            product.colors.length,
-            (index) => ColorDot(
-              color: product.colors[index],
-              isSelected: index == selectedColor,
+            widget.product.colors.length,
+            (index) => GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedColor = index;
+                });
+              },
+              child: Container(
+                margin: const EdgeInsets.only(right: 2),
+                padding: const EdgeInsets.all(8),
+                height: 40,
+                width: 40,
+                decoration: BoxDecoration(
+                  color: selectedColor == index
+                      ? const Color(0xFFFFECDF)
+                      : Colors.transparent,
+                  border: Border.all(
+                    color: selectedColor == index
+                        ? const Color(0xFFFF7643)
+                        : Colors.transparent,
+                  ),
+                  shape: BoxShape.circle,
+                ),
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: widget.product.colors[index],
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ),
             ),
           ),
           const Spacer(),
-          RoundedIconBtn(
-            icon: Icons.remove,
-            press: () {},
-          ),
-          const SizedBox(width: 20),
-          RoundedIconBtn(
-            icon: Icons.add,
-            showShadow: true,
-            press: () {},
-          ),
         ],
-      ),
-    );
-  }
-}
-
-class ColorDot extends StatelessWidget {
-  const ColorDot({
-    Key? key,
-    required this.color,
-    this.isSelected = false,
-  }) : super(key: key);
-
-  final Color color;
-  final bool isSelected;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(right: 2),
-      padding: const EdgeInsets.all(8),
-      height: 40,
-      width: 40,
-      decoration: BoxDecoration(
-        color: Colors.transparent,
-        border:
-            Border.all(color: isSelected ? kPrimaryColor : Colors.transparent),
-        shape: BoxShape.circle,
-      ),
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          color: color,
-          shape: BoxShape.circle,
-        ),
       ),
     );
   }
